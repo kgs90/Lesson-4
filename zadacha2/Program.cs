3c54e1de84e6695c038c4b3779ec11b40cf929a9
kgs90 <kgs90@inbox.ru>
2022-04-24 17:20:39 +0300

    zadacha2

---
+++ zadacha2/Program.cs
@@ -0,0 +1,15 @@
+﻿int number = 0, sum = 0;
+int i = 0;
+int lastNumber = 0;
+
+Console.WriteLine("Введите число: ");
+number = Convert.ToInt32(Console.ReadLine());
+lastNumber = number;
+
+while (number > 0) 
+{
+    sum = sum + number % 10;
+    number = number / 10;
+    i++;
+}
+Console.WriteLine("Сумма равна "  +  sum );
