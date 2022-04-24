4824ead47cb4f322b04eefce3ac6eb5850e7d5bc
kgs90 <kgs90@inbox.ru>
2022-04-24 17:30:40 +0300

    zadacha3

---
+++ zadacha3/Program.cs
@@ -0,0 +1,31 @@
+﻿int count = 0, max = 0, min = 0;
+int i = 0;
+int[] array = new int[100];
+Random rnd = new Random();
+
+Console.WriteLine("Количество элементов: ");
+count = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите  значения элементов массива: ");
+max = Convert.ToInt32(Console.ReadLine());
+min = Convert.ToInt32(Console.ReadLine());
+
+if (min > max) 
+{
+    i = max;
+    max = min;
+    min = i;
+}
+i = 0;
+
+Console.WriteLine("массив: ");
+genArray(count, min, max);
+
+void genArray(int N, int minimum, int maximum) 
+{ 
+    for (i = 0; i <= N; i++) 
+    {
+        array[i] = rnd.Next(minimum, maximum);
+        Console.Write(array[i] + " ");
+    }
+}
+Console.WriteLine();
