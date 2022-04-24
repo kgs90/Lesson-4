2e6cd2788481bbf2178430d78c744ad64e139091
kgs90 <kgs90@inbox.ru>
2022-04-24 17:09:05 +0300

    first task

---
+++ zadacha1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число и степень: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int pow = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(number + " в " + pow + " степени = " + userPow(number, pow));
+static int userPow(int number, int deg) 
+{
+    int result = 1;
+    for (int i = 1; i <= deg; i++) 
+    {
+        result = result * number;
+    }
+    return result;
+}
